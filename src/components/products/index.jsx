7286b88d62475e4a928c82ffc532069c9c016d7d
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-12-03 19:40:11 +0530

    Conditional Rendering - 1 Way

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -4,13 +4,23 @@ import ProductItem from "./components/product-item";
 
 
 function ProductList(props) {
+    const flag = true;
+
+
+
+
+
     // we will directly destructure is props function ProductList({name, city}) { }
     const {name, city, ListofProduct} = props;
     return (
         <div>
             <h3>ECommerce Project</h3>
             {/* <ProductItem /> */}
-            <h4>Name is {name} & city is {city}</h4>
+
+            {
+                flag ? (<h4>Name is {name} & city is {city}</h4>) : (<h4>Hello World</h4>)
+            }
+            
            <ul>
             {
                 ListofProduct.map((item, index)=>
