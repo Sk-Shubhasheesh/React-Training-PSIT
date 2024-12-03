96502c8aa0408da8cbad0290ec9168a3d0717579
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-12-03 19:45:04 +0530

    Conditional Rendering - 2 Way

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -5,28 +5,31 @@ import ProductItem from "./components/product-item";
 
 function ProductList(props) {
     const flag = true;
+    function renderTextBlock(getFlag) {
+        return getFlag ? <h4>Name is {name} & city is {city}</h4> : <h4>Hello World</h4>
 
+    }
 
 
 
 
     // we will directly destructure is props function ProductList({name, city}) { }
-    const {name, city, ListofProduct} = props;
+    const { name, city, ListofProduct } = props;
     return (
         <div>
             <h3>ECommerce Project</h3>
             {/* <ProductItem /> */}
 
             {
-                flag ? (<h4>Name is {name} & city is {city}</h4>) : (<h4>Hello World</h4>)
+                renderTextBlock(flag)
             }
-            
-           <ul>
-            {
-                ListofProduct.map((item, index)=>
-                <li key={index}>{item}</li>)
-            }
-           </ul>
+
+            <ul>
+                {
+                    ListofProduct.map((item, index) =>
+                        <li key={index}>{item}</li>)
+                }
+            </ul>
 
 
         </div>
