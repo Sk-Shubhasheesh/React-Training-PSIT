8166d8d31e7bf73c23b88123176d4048e372ee2e
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-12-03 18:36:57 +0530

    Class Based Component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ClassBasedComponent from './components/class-based-component'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Rect Js Concept 2024</h1>
+      <ClassBasedComponent />
+    </div>
+  )
+    
+}
+
+export default App
---
+++ src/components/class-based-component.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class ClassBasedComponent extends Component{
+    render(){
+        return <div>
+            <h3>Class Based component</h3>
+        </div>
+    }
+}
+
+export default ClassBasedComponent;
