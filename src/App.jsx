004563cfcda03a6724116ded1d5ec7f5a6f0cd95
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-12-03 18:59:43 +0530

    Nexted Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import ClassBasedComponent from './components/class-based-component'
 import FunctionalComponent from './components/functional-component'
+import ProductList from './components/products'
 
 function App() {
 
   return (
     <div>
       <h1>Rect Js Concept 2024</h1>
-      <ClassBasedComponent />
-      <FunctionalComponent />
+      {/* <ClassBasedComponent />
+      <FunctionalComponent /> */}
+
+      {/* Nexted Component Example - App -> product list -> product item -> button component
+ */}
+      <ProductList />
     </div>
   )
     
---
+++ src/components/products/index.jsx
@@ -0,0 +1,13 @@
+import ProductItem from "./components/product-item";
+
+const dummmyProductData = ['Product 1', 'Product 2', 'Product 3'];
+
+function ProductList() {
+    return (
+        <div>
+            <h3>ECommerce Project</h3>
+            <ProductItem />
+        </div>
+    )
+}
+export default ProductList;
