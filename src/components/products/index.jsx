2302171de420128a80795a794a8ee0e36abd7311
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-12-03 19:52:08 +0530

    Conditional Rendering - 3 Way

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -5,23 +5,23 @@ import ProductItem from "./components/product-item";
 
 function ProductList(props) {
     const flag = true;
-    function renderTextBlock(getFlag) {
-        return getFlag ? <h4>Name is {name} & city is {city}</h4> : <h4>Hello World</h4>
+    // function renderTextBlock(getFlag) {
+    //     return getFlag ? <h4>Name is {name} & city is {city}</h4> : <h4>Hello World</h4>
 
-    }
-
-
-
+    // }
+    
 
     // we will directly destructure is props function ProductList({name, city}) { }
     const { name, city, ListofProduct } = props;
+    const renderTextBlock = flag ? <h4>Name is {name} & city is {city}</h4> : <h4>Hello World</h4>
+
     return (
         <div>
             <h3>ECommerce Project</h3>
             {/* <ProductItem /> */}
 
             {
-                renderTextBlock(flag)
+                renderTextBlock
             }
 
             <ul>
