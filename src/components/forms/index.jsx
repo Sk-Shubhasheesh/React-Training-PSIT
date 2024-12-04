f6f1978c2829248b85472c89a6dbaebc3e556cee
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-12-04 19:40:28 +0530

    form submission

--- src/components/forms/index.jsx
+++ src/components/forms/index.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function FormComponent() {
     const [nameValue, setNameValue] = useState("")
+    const [emailValue, setEmailValue] = useState("")
 
     function handleInputChange(event) {
         // const {value} = event.target
@@ -11,15 +12,23 @@ function FormComponent() {
 
     }
 
+    function handleEmailChange(event){
+        console.log(event);
+        setEmailValue(event.target.value)
+
+    }
+
     function handleSubmit(event){
         // stop the behaviour
         event.preventDefault();
 
         // call api here and pass the name value
-        console.log(nameValue, "nameValue");
+        console.log(nameValue, emailValue, "nameValue");
         
 
     }
+
+    
 
     return (
         <div>
@@ -27,6 +36,8 @@ function FormComponent() {
             <form onSubmit={handleSubmit}>
                 <input value={nameValue} name="name" id="name" placeholder="Enter you name"
                     onChange={handleInputChange} />
+
+                <input type="email" name="email" id="email" placeholder="Enter Your Email" onChange={handleEmailChange} value={emailValue}/>
 
                 <button type="submit">Submit</button>
             </form>
