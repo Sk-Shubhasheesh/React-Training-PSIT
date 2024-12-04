938862bdbd5778f8cc1952f13a21e98a0e8451ab
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-12-04 11:37:18 +0530

    useEffect

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1,16 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductItem from "./components/product-item";
 
 
 
 
 function ProductList(props) {
-    const[flag, setFlag] = useState(false)
+    
     const { name, city, ListofProduct } = props;
+
+    const[flag, setFlag] = useState(false)
+    const[count, setCount] = useState(0)
+    const[changeStyle, setChangeStyle] = useState(false  )
 
     function handleToggleText(){
         setFlag(!flag)
     }
+
+    function handleIncreaseCount(){
+        setCount(count +1);
+    }
+
+    useEffect(()=>{
+        console.log("Run only page load");
+        
+    },[]) // This is run on page load once
+
+    useEffect(()=>{
+        console.log("count changes");
+        if(count == 10) setChangeStyle(true)
+        
+    },[count]) // it render when count is change
+
+
+
     return (
         <div>
             <h3>ECommerce Project</h3>
@@ -20,6 +42,11 @@ function ProductList(props) {
             {
                 flag ?  (<h4>Name is {name} & city is {city}</h4>) : (<h4>Hello World</h4>)
             }
+
+            <div>
+                <button style={{backgroundColor: changeStyle ?'black' : 'white',color:changeStyle?'white':'black'}} onClick={handleIncreaseCount}>Increase Count</button>
+                <p>Count is {count}</p>
+            </div>
 
             <ul>
                 {
