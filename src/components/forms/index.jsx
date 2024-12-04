44621d68ef6eb008dd36e956942ac542e9bde8ee
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-12-04 19:26:31 +0530

    work with simple form

--- src/components/forms/index.jsx
+++ src/components/forms/index.jsx
@@ -11,12 +11,29 @@ function FormComponent() {
 
     }
 
+    function handleSubmit(event){
+        // stop the behaviour
+        event.preventDefault();
+
+        // call api here and pass the name value
+        console.log(nameValue, "nameValue");
+        
+
+    }
+
     return (
         <div>
             <h1>Form</h1>
+            <form onSubmit={handleSubmit}>
+                <input value={nameValue} name="name" id="name" placeholder="Enter you name"
+                    onChange={handleInputChange} />
 
-            <input value={nameValue} name="name" id="name" placeholder="Enter you name"
-                onChange={handleInputChange} />
+                <button type="submit">Submit</button>
+            </form>
+
+
+
+
         </div>
     )
 
